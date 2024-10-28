1e3b5a45e74d444117f85f55f4ed72231bbaeebd
ryan chupick <ryan.chupick@gmail.com>
2024-10-28 19:20:51 -0400

    borrow the with construction for echo/server.ex as that works for some reason

--- lib/protoh/echo/server.ex
+++ lib/protoh/echo/server.ex
@@ -5,37 +5,13 @@ defmodule Protoh.Echo.Server do
   def start(socket, _opts), do: serve(socket)
 
   defp serve(socket) do
-    socket
-    |> read_line()
-    |> write_line(socket)
-
-    serve(socket)
-  end
-
-  defp read_line(socket) do
-    {:ok, data} = :gen_tcp.recv(socket, 0)
-    data
-  end
-
-  defp write_line(line, socket) do
-    :gen_tcp.send(socket, line)
+    with {:ok, data} <- :gen_tcp.recv(socket, 0),
+         :ok <- :gen_tcp.send(socket, data) do
+      serve(socket)
+    else
+      _error ->
+        Logger.debug("#{inspect(__MODULE__)}: Client Closed (#{inspect(socket)})")
+        :gen_tcp.close(socket)
+    end
   end
 end
-
-# defmodule Protoh.Echo.Server do
-#   require Logger
-#
-#   def start(socket), do: serve(socket)
-#   def start(socket, _opts), do: serve(socket)
-#
-#   defp serve(socket) do
-#     with {:ok, data} <- :gen_tcp.recv(socket, 0),
-#          :ok <- :gen_tcp.send(socket, data) do
-#       serve(socket)
-#     else
-#       _error ->
-#         Logger.debug("#{inspect(__MODULE__)}: Client Closed (#{inspect(socket)})")
-#         :gen_tcp.close(socket)
-#     end
-#   end
-# end
