49ec8a916698db2d5d12903f6ad6272708244030
ryan chupick <ryan.chupick@gmail.com>
2024-10-28 21:14:54 -0400

    wip

--- lib/protoh/prime/server.ex
+++ lib/protoh/prime/server.ex
@@ -5,13 +5,14 @@ defmodule Protoh.Prime.Server do
   def start(socket, _opts), do: serve(socket)
 
   defp serve(socket) do
-    with {:ok, data} <- :gen_tcp.recv(socket, 0),
-         :ok <- :gen_tcp.send(socket, data) do
-      serve(socket)
-    else
-      _error ->
-        Logger.debug("#{inspect(__MODULE__)}: Client Closed (#{inspect(socket)})")
-        :gen_tcp.close(socket)
-    end
+    IO.inspect(:gen_tcp.recv(socket, 0))
+    #   with {:ok, data} <- :gen_tcp.recv(socket, 0),
+    #        :ok <- :gen_tcp.send(socket, data) do
+    #     serve(socket)
+    #   else
+    #     _error ->
+    #       Logger.debug("#{inspect(__MODULE__)}: Client Closed (#{inspect(socket)})")
+    #       :gen_tcp.close(socket)
+    #   end
   end
 end
