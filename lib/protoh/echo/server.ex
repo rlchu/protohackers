72db0b241d6f86da2ff5f6560453831e9a3ac64d
ryan chupick <ryan.chupick@gmail.com>
2024-10-28 20:59:15 -0400

    ok just use with macro

--- lib/protoh/echo/server.ex
+++ lib/protoh/echo/server.ex
@@ -1,30 +1,16 @@
-defmodule Protoh.Echo.Server do
+defmodule Protohacker.Echo.Server do
   require Logger
 
-  def start(socket), do: serve(socket)
   def start(socket, _opts), do: serve(socket)
 
   defp serve(socket) do
-    socket
-    |> read_line()
-    |> write_line(socket)
-
-    serve(socket)
-  end
-
-  defp read_line(socket) do
-    case :gen_tcp.recv(socket, 0) do
-      {:ok, data} -> data
-      _ -> close(socket)
+    with {:ok, data} <- :gen_tcp.recv(socket, 0),
+         :ok <- :gen_tcp.send(socket, data) do
+      serve(socket)
+    else
+      _error ->
+        Logger.debug("#{inspect(__MODULE__)}: Client Closed (#{inspect(socket)})")
+        :gen_tcp.close(socket)
     end
-  end
-
-  defp write_line(line, socket) do
-    :gen_tcp.send(socket, line)
-  end
-
-  defp close(socket) do
-    Logger.debug("#{inspect(__MODULE__)}: Client Closed (#{inspect(socket)})")
-    :gen_tcp.close(socket)
   end
 end
