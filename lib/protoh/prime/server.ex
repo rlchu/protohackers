123cc2494a75ced3b7e9e43568c70c218dd0eaae
ryan chupick <ryan.chupick@gmail.com>
2024-10-28 23:23:40 -0400

    try prime library and fix case for malformed incoming data

--- lib/protoh/prime/server.ex
+++ lib/protoh/prime/server.ex
@@ -5,14 +5,38 @@ defmodule Protoh.Prime.Server do
   def start(socket, _opts), do: serve(socket)
 
   defp serve(socket) do
-    IO.inspect(:gen_tcp.recv(socket, 0))
+    dbg()
+
+    data =
+      case :gen_tcp.recv(socket, 0) do
+        {:ok, data} -> data
+        _ -> 0
+      end
+
+    {:ok, %{"method" => _method, "number" => number}} = Jason.decode(data)
+
+    is_prime = check_prime(number)
+
+    response = %{method: "isPrime", prime: is_prime}
+
+    {:ok, response_data} = Jason.encode(response)
+    :ok = :gen_tcp.send(socket, response_data)
+    dbg()
+    serve(socket)
+
     #   with {:ok, data} <- :gen_tcp.recv(socket, 0),
-    #        :ok <- :gen_tcp.send(socket, data) do
+    #         json_data <- get_json(data)
+    #         response_json <- check_prime(json_data)
+    #        :ok <- :gen_tcp.send(socket, response_json) do
     #     serve(socket)
     #   else
     #     _error ->
     #       Logger.debug("#{inspect(__MODULE__)}: Client Closed (#{inspect(socket)})")
     #       :gen_tcp.close(socket)
     #   end
+  end
+
+  defp check_prime(number) do
+    Prime.test(number)
   end
 end
